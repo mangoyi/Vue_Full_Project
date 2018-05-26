fa740f8be35ad2242a4d90bbdfedad667747f36b
mangoyi <mangoyiy@163.com>
2018-05-26 19:27:51 +0800

    taskRelease

--- src/views/components/taskmanage/taskDetails/TaskDetails.vue
+++ src/views/components/taskmanage/taskDetails/TaskDetails.vue
@@ -10,10 +10,6 @@
                     <el-date-picker size="large" v-model="startDate" type="date" placeholder="选择日期时间" value-format="yyyy-MM-dd">
                     </el-date-picker>
                 </div>
-                <!-- <div class="col-md-3 search-field">
-                    <div class="label" style="left:0px;">模板名称：</div>
-                    <el-input v-model="input" placeholder="请输入内容"></el-input>
-                </div> -->
                 <div class="col-md-3 search-field">
                     <div style="left: 0px" class="label">结束日期：</div>
                     <el-date-picker size="large" v-model="endDate" type="date" placeholder="选择日期时间" value-format="yyyy-MM-dd">
@@ -31,11 +27,6 @@
                         Your browser does not support the audio element.
                     </audio>
                 </div>
-                <!-- <div class="col-md-1 search-field search-field_controls">
-                    <router-link class="btn btn-success" :to="'/message/addModal'">
-                        新增
-                    </router-link>
-                </div> -->
             </div>
             <div class="row">
                 <div class="col-lg-12">
@@ -56,18 +47,20 @@
                         <tbody>
                             <tr v-for="item in cartList" :key="item.id">
                                 <td>{{item.sequence}}</td>
-                                <td>{{item.line}}</td>
-                                <td>{{item.callStatus}}</td>
-                                <td>{{item.inCall}}</td>
-                                <td>{{item.outCall}}</td>
+                                <td>{{item.number}}</td>
                                 <td>{{item.startTime}}</td>
                                 <td>{{item.duration}}</td>
                                 <td>{{item.RobotId}}</td>
+                                <td>{{item.LaborId}}</td>
                                 <td>
                                     <a class="i-wrap" @click="listen(item)" style="color: black">
                                         <i class="fa fa-microphone"  v-bind:class="{'text-success': item.recordFlag }"></i>
                                     </a>
                                 </td>
+                                <td>
+                                    <el-button class="btn btn-primary" plain @click="openDialog(item)" >查看聊天对话</el-button>
+                                </td>
+                                <td>{{item.flag}}</td>
                             </tr>                                          
                         </tbody>
                     </table>
@@ -83,7 +76,7 @@
                         </el-pagination>
                     </div>
                     <div class="info" v-show="cartList.length == 0">
-                        请根据条件搜索通话记录
+                        请根据条件搜索记录
                     </div>
                 </div>
             </div>
@@ -98,13 +91,20 @@
                 <el-button type="primary" @click="centerDialogVisible = false">确 定</el-button>
             </span>
         </el-dialog>
+
+        <!-- <el-button
+            plain
+            @click="open12">
+            使用 HTML 片段
+        </el-button>     -->
     </div>
 </template>
 
 <script>
-import { Pagination, DatePicker, Button, Input} from "element-ui";
+import { Pagination, DatePicker, Button, Input, Notification} from "element-ui";
 import axios from 'axios';
-import data from "@/../mock/mock-callRecords.json";                             // mock json
+import data from "@/../mock/mock-taskDetails.json";                             // mock json
+import chatdata from "@/../mock/mock-chatDialog.json";                          // mock json
 
 /* eslint-disable */
 export default {
@@ -113,14 +113,13 @@ export default {
             startDate: "",
             endDate: "",
             number: "",
-            // [false, false, false, false, false, false, false, false, false, false],
             listenFlag: false,
             recordSrc: "",
             centerDialogVisible: false,
             currentPage: 1,
             pageSize: 10,
-            // input: '',
-            cartList: []
+            cartList: [],
+            dialog: ""
         };
     },
     mounted() {
@@ -130,8 +129,7 @@ export default {
         searchList() {
             let number    = this.number;
             let startDate = this.startDate;
-            let endDate   = this.endDate + 1;
-            console.log(endDate)
+            let endDate   = this.endDate;
             if(number == false && startDate == false && endDate == false) {
                 alert('请填写至少一个搜索条件');
                 return;
@@ -185,6 +183,43 @@ export default {
             }
             item.recordFlag = !item.recordFlag;
             console.log(item.recordFlag);
+        },
+
+        // open 对话内容
+        openDialog(item) {
+            let taskId = item.taskId;
+            let number = item.number;
+
+            // 请求聊天记录数据 chatDialog
+            /*
+            axios.post('/home/chatDialog', {
+                "taskId": taskId,
+                "number": number
+            }).then(function(response) {
+                let res = response.data;
+                if(res.status == 0) {
+                    console.log("数据请求成功");
+                }
+            });
+            */
+            let data = chatdata.data.list;
+            let chatDetail = "";
+            let chatDetailArr = [];
+            data.forEach(item => {
+                if(item.sayStatus == 0) {           // 机器人问
+                    chatDetail += '<div class="say" style="position: relative;width: 276px;background: #ebebeb;overflow: hidden;"><div class="say_svg common_svg"  style="position: absolute; width: 14px; height: 14px;top: 3px;left: 0px;"><svg viewBox="0 0 120 180" id="trigon-left" width="100%" height="100%" ><path fill="#fff" d="M104.555,178.368l-1.313-1.013l-97.863-76.5C2.064,98.269,0,94.387,0,90.056c0-4.332,2.126-8.212,5.378-10.8l97.676-76.443l1.626-1.294C106.243,0.563,108.119,0,110.12,0c5.44,0,9.88,4.162,9.88,9.338l0,0v161.324l0,0c0,5.175-4.439,9.338-9.88,9.338C108.057,180,106.118,179.381,104.555,178.368L104.555,178.368z"></path></svg></div><div class="say_text common_text" style="max-width: 220px;padding: 6px; border: 1px solid #d9d9d9;border-radius: 8px;font-size: 14px;line-height: 18px;color: #333;word-break: break-all;margin-left: 10px; background: #fff;">'+item.sayText+'</div></div>';
+                } else if(item.sayStatus == 1) {                            // 客户回答
+                    chatDetail += '<div class="replay" style="position: relative;width: 276px;background: #ebebeb;overflow: auto;"><div class="replay_svg common_svg"  style="position: absolute; width: 14px; height: 14px; right: 0px;top: 3px;"><svg viewBox="0 0 120 180" id="trigon-left" width="100%" height="100%" ><path fill="#9fe658" d="M15.446,1.631l1.313,1.013l97.863,76.5c3.313,2.587,5.378,6.469,5.378,10.8c0,4.332-2.126,8.212-5.378,10.8l-97.676,76.443l-1.626,1.294C13.757,179.438,11.881,180,9.88,180C4.44,180,0,175.838,0,170.662l0,0V9.337l0,0C0,4.163,4.44,0,9.88,0C11.944,0,13.882,0.619,15.446,1.631L15.446,1.631z"></path></svg></div><div class="replay_text common_text" style="max-width: 220px;padding: 6px; border: 1px solid #d9d9d9;border-radius: 8px;font-size: 14px;line-height: 18px;color: #333;word-break: break-all;float: right;margin-right: 10px; background: #9fe658;">'+item.sayText+'</div></div>';
+                }
+            });
+            console.log(chatDetail);               // 聊天的html文件
+
+            this.$notify({
+                title: '聊天内容',
+                duration: 0,
+                dangerouslyUseHTMLString: true,
+                message: chatDetail
+            });
         }
 
     }
@@ -208,4 +243,19 @@ export default {
         text-align: center;
         color: #666;
     }
+
+    .table tr:hover>td {
+        background-color: #f5f7fa;
+    }
+    .table tr td {
+        transition: background-color .25s ease;
+    }
+
+    .el-notification__group .left {
+        background: salmon;
+    }
+    .right {
+        background: rebeccapurple;
+    }
+
 </style>
--- src/views/components/taskmanage/taskRelease/TaskRelease.vue
+++ src/views/components/taskmanage/taskRelease/TaskRelease.vue
@@ -32,28 +32,56 @@
                     </router-link>
                 </div> -->
                 <div class="col-md-3 search-field">
-                    <div class="label" style="left:0px;">任务名称：</div>
+                    <div class="label" style="left:0px;top: 4px;">任务名称：</div>
                     <el-input v-model="input" placeholder="请输入内容"  :disabled="true"></el-input>
                 </div>
-                <div class="col-md-3">
-                    <el-upload
-                        class="upload-demo"
-                        ref="upload"
-                        action="https://jsonplaceholder.typicode.com/posts/"
-                        :on-preview="handlePreview"
-                        :on-remove="handleRemove"
-                        :file-list="fileList"
-                        :auto-upload="false">
-                        <el-button slot="trigger" size="small" type="primary">选取压缩文件</el-button>
-                        <el-button style="margin-left: 10px;" size="small" type="success" @click="submitUpload">上传到服务器</el-button>
-                        <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>
-                    </el-upload>
+                <div class="col-md-12 search-field">
+                    <div class="label" style="left:0px;top: 24px;">选择文件：</div>
+                    
+                    <div class="col-md-3 upload-wrap">
+                        <el-upload
+                            class="upload-file"
+                            ref="upload"
+                            action="https://jsonplaceholder.typicode.com/posts/"                                        
+                            :before-upload = "beforeAvatarUpload"
+                            :on-preview="handlePreview"
+                            :on-remove="handleRemove"
+                            :limit = 1
+                            :file-list="fileList"
+                            :auto-upload="false">
+                            <el-button slot="trigger" size="small" type="primary">选取压缩文件</el-button>
+                            <el-button style="margin-left: 10px;" size="small" type="success" @click="submitUpload">上传到服务器</el-button>
+                            <div slot="tip" class="el-upload__tip">只能上传单个zip文件，且不超过2MB</div>
+                        </el-upload>
+                    </div>
                 </div>
-                <div class="col-md-8">
-                    <template>
-                        <el-transfer v-model="value1" :data="data"></el-transfer>
-                    </template>
+                <div class="col-md-12 search-field" style="margin-bottom: 40px;">
+                    <div class="label" style="left: -11px;">机器人坐席：</div>
+                    <div class="col-md-5 transfer-wrap">
+                        <template>
+                            <el-transfer 
+                                v-model="checkedTransferData" 
+                                :data="transferData"
+                                :titles="['所有机器人', '工作机器人']"
+                                :button-texts="['移除', '添加']"
+                                @change="handleChange"
+                            ></el-transfer>
+                        </template>                        
+                    </div>
                 </div>
+                <div class="col-md-12 search-field">
+                    <div class="label" style="left: -11px">人工座席：</div>
+                    <div class="col-md-5 transfer-wrap">
+                        <el-transfer 
+                            v-model="checkedTransferData1" 
+                            :data="transferData1"
+                            :titles="['所有员工', '工作员工']"
+                            :button-texts="['移除', '添加']"
+                            @change="handleChange1"
+                        ></el-transfer>
+                    </div>
+                </div>
+
             </div>
         </div>
 
@@ -73,21 +101,24 @@
 import { Pagination, DatePicker, Button, Input} from "element-ui";
 import axios from 'axios';
 import data from "@/../mock/mock-callRecords.json";                             // mock json
+import robotList from "@/../mock/mock-robotList.json";                          // mock json
+import laborList from "@/../mock/mock-laborList.json";                          // mock json
 
 /* eslint-disable */
 export default {
     data() {
-        const generateData = _ => {
-            const data = [];
-            for (let i = 1; i <= 15; i++) {
-            data.push({
-                key: i,
-                label: `备选项 ${ i }`,
-                disabled: i % 4 === 0
-            });
-            }
-            return data;
-        };
+        // const generateData = _ => {
+        //     const transferData = [];
+        //     for (let i = 1; i <= 15; i++) {
+        //         transferData.push({
+        //             key: i,
+        //             label: `机器人 ${ i }`,
+        //             disabled: i % 4 === 0
+        //         });
+        //     }
+        //     console.log(transferData);
+        //     return transferData;
+        // };
 
         return {
             startDate: "",
@@ -100,14 +131,38 @@ export default {
             currentPage: 1,
             pageSize: 10,
             input: '',
-            fileList: [{name: 'food.jpeg', url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100'}, {name: 'food2.jpeg', url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100'}],
+            fileList: [],
             cartList: [],
 
-            data: generateData(),
-            value1: [1, 4]
+            // transferData: generateData(),
+            transferData: [],
+            checkedTransferData: [],
+
+            // labor data
+            transferData1: [],
+            checkedTransferData1: []
         };
     },
+    mounted() {
+        this.initRobot();
+        this.initLabor();
+    },
     methods: {
+        beforeAvatarUpload(file){
+            // const isJPG = file.type === 'application/zip';
+
+            const testmsg=file.name.substring(file.name.lastIndexOf('.')+1);  
+            const extention = testmsg === 'zip';
+            const isLt2M = file.size / 1024 / 1024 < 2;
+            console.log(extention);
+            if (!extention) {
+                this.$message.error('上传文件格式只能是 zip 格式!');
+            }
+            if (!isLt2M) {
+                this.$message.error('上传文件不能超过 2MB!');
+            }
+            return extention && isLt2M;
+        },
         submitUpload() {
             this.$refs.upload.submit();
         },
@@ -116,7 +171,131 @@ export default {
         },
         handlePreview(file) {
             console.log(file);
+        },
+
+
+        initRobot() {
+
+            // 请求所有机器人
+            /*
+            axios.post("/home/robotList", {}).then((response) => {
+                let res = response.data;                    // 获取所有机器人
+                if (res.status == 0) {
+                    console.log("请求成功");
+                }
+            });
+            */
+            let data = robotList.data.list;                      // 所有机器人
+            let thatcheckedTransferData = this.checkedTransferData;
+            data.forEach((item, index) => {
+                this.transferData.push(
+                    (function() {
+                        console.log(item);
+                        if ( item.robotStatus == 1 && item.robotTask != 10001) {                    // 机器人在工作，并且不是在当前这个任务中。 所以不能选择
+                            return {
+                                key: index,
+                                label: "" + item.robotId,
+                                disabled: true
+                            }
+                        } else if ( item.robotStatus == 1 && item.robotTask == 10001) {
+                            thatcheckedTransferData.push(index);                                // 机器人在工作，并且在当前这个任务中。所以显示在右侧
+                        } 
+                        return {
+                            key: index,                                                         // 自增, 所有机器人(空闲机器人)
+                            label: "" + item.robotId,
+                            disabled: false
+                        }
+                    })()
+                );
+            });
+            // console.log("data" + this.transferData);
+        },
+        handleChange() {
+            // 机器人坐席变化时触发的事件  计算变化后的checkedTransferData,  （移除机器人，增加机器人）
+            let workRobotList = this.checkedTransferData;
+            let workRobotId = [];
+            workRobotList.forEach(item => {
+                workRobotId.push(Number(this.transferData[item].label));            // 将对应剩下的机器人ID存放到workRobotId数组中
+            })
+
+            /*
+            axios.post("/home/changetaskRobot/", {
+                taskId: 1011,
+                workRobot: workRobotId                                              // 将对应剩下的机器人ID传递给后台
+            }).then((response) => {
+                let res = response.data;
+                if(res.status == 0) {
+                    console.log("修改成功");
+                }
+            });
+            */
+
+        },
+
+        // labor
+        initLabor() {
+            // 请求所有员工
+            /*
+            axios.post("/home/laborList", {}).then((response) => {
+                let res = response.data;                        // 获取所有员工
+                if (res.status == 0) {
+                    console.log("请求成功");
+                }
+            });
+            */
+            
+            let data = laborList.data.list;                      // 所有员工
+            let thatcheckedTransferData = this.checkedTransferData1;
+            data.forEach((item, index) => {
+                this.transferData1.push(
+                    (function() {
+                        console.log(item);
+                        if ( item.laborStatus == 1 && item.laborTask != 10001) {                    // 员工在工作，并且不是在当前这个任务中。 所以不能选择
+                            return {
+                                key: index,
+                                label: "" + item.laborId,
+                                disabled: true
+                            }
+                        } else if ( item.laborStatus == 1 && item.laborTask == 10001) {
+                            thatcheckedTransferData.push(index);                                // 员工在工作，并且在当前这个任务中。所以显示在右侧
+                        } 
+                        return {
+                            key: index,                                                         // 自增, 所有员工(空闲员工)
+                            label: "" + item.laborId,
+                            disabled: false
+                        }
+                    })()
+                );
+            });
+            // console.log("data" + this.transferData1);      
+        },
+        handleChange1() {
+            // 员工坐席变化时触发的事件  计算变化后的checkedTransferData1,  （移除员工，增加员工）
+            let workLaborList = this.checkedTransferData1;
+            let workLaborId = [];
+            workLaborList.forEach(item => {
+                workLaborId.push(Number(this.transferData1[item].label));            // 将对应剩下的员工ID存放到workLaborId数组中
+            })
+            workLaborId.forEach(item => {
+                console.log(item);
+            })
+
+            /*
+            axios.post("/home/changetaskLabor/", {
+                taskId: 1011,
+                workLabor: workLaborId                                              // 将对应剩下的员工ID传递给后台
+            }).then((response) => {
+                let res = response.data;
+                if(res.status == 0) {
+                    console.log("修改成功");
+                }
+            });
+            */
+
         }
+
+
+
     }
 };
 </script>
@@ -137,5 +316,14 @@ export default {
         padding-top: 14px;
         text-align: center;
         color: #666;
+    }
+    
+    .upload-wrap {
+        margin-top: 20px;
+        margin-bottom: 20px;
+        padding-left: 0px;
+    }
+    .transfer-wrap {
+        padding-left: 0px;
     }
 </style>
