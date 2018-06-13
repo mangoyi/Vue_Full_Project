0d0080bd69d4229082dbea1a5d7bb36cf4ef9292
mangoyi <mangoyiy@163.com>
2018-06-13 18:18:16 +0800

    voice

--- src/views/components/taskmanage/taskDetails/TaskDetails.vue
+++ src/views/components/taskmanage/taskDetails/TaskDetails.vue
@@ -58,6 +58,7 @@
                                     </a>
                                 </td>
                                 <td>
+                                    <!-- <el-button class="btn btn-primary" plain @click="openDialog(item.Id)" >查看聊天对话</el-button> -->
                                     <el-button class="btn btn-primary" plain @click="openDialog(item.Id)" >查看聊天对话</el-button>
                                 </td>
                                 <td>{{item.flag}}</td>
@@ -82,6 +83,7 @@
             </div>
         </div>
 
+        <!-- 提示 -->
         <el-dialog title="提示" :modal-append-to-body="false" :visible.sync="centerDialogVisible" width="20%" center>
             <div class="text-center">
                 <span>确定要取消吗?</span>
@@ -91,14 +93,47 @@
                 <el-button type="primary" @click="centerDialogVisible = false">确 定</el-button>
             </span>
         </el-dialog>
+
+        <!-- 聊天对话 -->
+        <el-dialog title="聊天内容" :modal-append-to-body="false" :visible.sync="recordDialog" width="40%" center>
+            <div class="yi-wrap">
+                <div class="row mb-4" v-for="(item, index) in recordList" :key="index">
+                    <div class="say" v-if="item.infoType == 'R'">
+                        <span class="g-time">{{item.startTime}}</span>
+                        <div class="say-text common-text">{{item.sayText}}<span style="padding-left: 12px;font-size: 12px;color: #38F;">"匹配规则:"{{item.mathRule}}</span></div>
+                    </div>
+                    <div class="replay" v-if="item.infoType == 'M'">
+                        <span class="g-time replay-time">{{item.startTime}}</span>
+                        <div class="replay-content common-text" @click="playVoice(`audio_${index}`, item)">
+                            <img src="../../../../../static/img/voice.png" alt="error" :class="{voice: item.imgFlag}" :ref="`img_${index}`">
+                            <span class="replay-text">{{item.sayText}}</span>    
+                            <audio preload="none" class="wav" :ref="`audio_${index}`" @ended="ended(item)">
+                                <source :src="'http://www.zzbn.cn:8090'+item.voiceSrc">
+                            </audio>
+                            <!-- <audio id="audioPlay" :src="isPlay" @canplay="playing()" @ended="ended()" v-el:audio> </audio> -->
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+            <span slot="footer" class="dialog-footer">
+                <!-- <el-button type="primary" @click="recordDialog = false">确 定</el-button> -->
+                    <audio src="http://global.res.btows.com/monitor/2018/05/12/force-13927784678-705-20180512-183058-1526121058.102223_2.wav" controls="controls" class="" preload="none">
+                        Your browser does not support the audio element.
+                    </audio>
+            </span>
+        </el-dialog>
+
+
+
     </div>
 </template>
 
 <script>
-import { Pagination, DatePicker, Button, Input, Notification} from "element-ui";
+import { Pagination, DatePicker, Button, Input, Notification, MessageBox} from "element-ui";
 import axios from 'axios';
 import data from "@/../mock/mock-taskDetails.json";                             // mock json
-import chatdata from "@/../mock/mock-chatDialog.json";                          // mock json
+// import chatdata from "@/../mock/mock-chatDialog.json";                          // mock json
 import taskDetailSrv from "@/../src/views/services/taskDetail.service.js";           
 
 
@@ -112,11 +147,16 @@ export default {
             // listenFlag: false,
             recordSrc: "",                                                      // 录音地址
             centerDialogVisible: false,
+            recordDialog: false,
             currentPage: 1,
             pageSize: 10,
             totalPageNum: 1,
             detailList: [],
             // dialog: ""
+
+            // 聊天内容
+            showVoice: false,
+            recordList: []
         };
     },
     // mounted() {
@@ -126,8 +166,6 @@ export default {
         next(vm => {
             taskDetailSrv.taskDetail(vm.$route.query.taskId, vm.startDate, vm.endDate, vm.number, vm.currentPage, vm.pageSize).then(resp => {
                 vm.detailList = resp.data.list;
-                console.log("]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]");
-                console.log(resp);
                 vm.totalPageNum = resp.data.totalPageNum;
             }, err => {
                 vm.$message.error(err.msg);
@@ -147,52 +185,6 @@ export default {
 
         },
 
-        // searchList() {
-        //     let number    = this.number;
-        //     let startDate = this.startDate;
-        //     let endDate   = this.endDate;
-        //     let _this     = this;
-        //     if(!startDate && !endDate && !number) {                                             // 三个条件都为空
-        //         alert('请填写至少一个搜索条件');
-        //         return;
-        //     } else {
-        //         axios.post("/api/api/task/searchTaskList", {                                   /// ?????????????????
-        //             number     : number,
-        //             startDate  : startDate,
-        //             endDate    : endDate
-        //         }).then((response) => {
-        //             let res = response.data;
-        //             if( res.status == 0) {       
-        //                 _this.detailList = res.data.list;
-        //             }
-        //         });
-        //     }
-        // },
-        // handleCurrentChange(val) {
-        //     alert("当前页:"+`${val}`+", 当前页个数:"+this.pageSize )
-        //     let currentPage = `${val}`;
-        //     let pageSize = this.pageSize;
-        //     this.init(currentPage, pageSize);           
-        // },
-        // init(currentPage, pageSize) {
-        //     let taskId = this.$route.query.taskId;
-        //     let _this = this;
-        //     axios.post("/api/api/task/taskDetail", {                                            // 初始化任务明细内容
-        //         taskId: taskId,
-        //         currentPage: currentPage == undefined ? 1 : currentPage,
-        //         pageSize: pageSize == undefined ? 10 : pageSize
-        //     }).then(function(response) {
-        //         let res = response.data;
-        //         if (res.status == 0) {
-        //             _this.detailList = res.data.list;    
-        //             _this.totalPage = res.data.totalPageNum;
-        //         }
-        //     }).catch(function(error) {
-        //         alert(error);
-        //     });
-        // },
-
-
         // 录音
         listen(item) {
             if (!item.recordPlayState) {                                                // 说明是播放
@@ -206,60 +198,27 @@ export default {
             console.log(item.recordPlayState);
         },
 
-        // open 对话内容
-        openDialog(id) {    
-            let chatDetail    = "";                                 // 聊天html字符串
-            let chatDetailArr = [];
-            // let _this         = this;
-            
-            taskDetailSrv.dialog(1).then(resp => {
-                let data = resp.data;
-                if (data == 1) {
-                    chatDetail = "<p>当前无聊天对话</p>";
-                } else {
-                    data.forEach(item => {
-                        if(item.infoType == "R") {                                   // 机器人问
-                            chatDetail += '<div class="say" style="position: relative;width: 420px;background: #ebebeb;overflow: hidden; padding: 5px 0;"><div class="say_svg common_svg"  style="position: absolute; width: 14px; height: 14px;top: 8px;left: 0px;"><svg viewBox="0 0 120 180" id="trigon-left" width="100%" height="100%" ><path fill="#fff" d="M104.555,178.368l-1.313-1.013l-97.863-76.5C2.064,98.269,0,94.387,0,90.056c0-4.332,2.126-8.212,5.378-10.8l97.676-76.443l1.626-1.294C106.243,0.563,108.119,0,110.12,0c5.44,0,9.88,4.162,9.88,9.338l0,0v161.324l0,0c0,5.175-4.439,9.338-9.88,9.338C108.057,180,106.118,179.381,104.555,178.368L104.555,178.368z"></path></svg></div><div class="say_text common_text" style="max-width: 390px;padding: 6px; border: 1px solid #d9d9d9;border-radius: 8px;font-size: 14px;line-height: 18px;color: #333;word-break: break-all;margin-left: 10px; background: #fff;">'+item.speakText+'<span style="font-size: 12px; color: #38F; padding-left: 10px;">'+item.mathRule+'</span>'+'</div></div>';
-                        } else if(item.infoType == "M") {                            // 客户回答
-                            chatDetail += '<div class="replay" style="position: relative;width: 420px;background: #ebebeb;overflow: auto; padding: 5px 0;"><div class="replay_svg common_svg"  style="position: absolute; width: 14px; height: 14px; right: 0px;top: 8px;"><svg viewBox="0 0 120 180" id="trigon-left" width="100%" height="100%" ><path fill="#9fe658" d="M15.446,1.631l1.313,1.013l97.863,76.5c3.313,2.587,5.378,6.469,5.378,10.8c0,4.332-2.126,8.212-5.378,10.8l-97.676,76.443l-1.626,1.294C13.757,179.438,11.881,180,9.88,180C4.44,180,0,175.838,0,170.662l0,0V9.337l0,0C0,4.163,4.44,0,9.88,0C11.944,0,13.882,0.619,15.446,1.631L15.446,1.631z"></path></svg></div><div class="replay_text common_text" style="max-width: 390px;padding: 6px; border: 1px solid #d9d9d9;border-radius: 8px;font-size: 14px;line-height: 18px;color: #333;word-break: break-all;float: right;margin-right: 10px; background: #9fe658;">'+item.speakText+'<span style="font-size: 12px; color: #38F">'+item.mathRule+'</span>'+'</div></div>';
-                        }
-                    }); 
-                }
-                this.$notify({
-                    title: "聊天内容",
-                    duration: 0,
-                    dangerouslyUseHTMLString: true,
-                    message: chatDetail
+        // open 对话
+        openDialog(id) {
+            this.recordDialog = true;
+            taskDetailSrv.dialog("1").then(resp => {
+                let data = resp.data.list;
+                data.forEach((item) => {
+                    item["imgFlag"] = false;
                 })
+                this.recordList = data;
             }, err => {
                 this.$message.error(err.msg);
             })
-
-
-            // 请求聊天对话数据 searchDialog
-            // axios.post('/api/api/task/searchDialog', {
-            //     Id:1
-            // }).then(function(response) {
-            //     if (response.data == 1) {                           // 当前无聊天对话
-            //         chatDetail = "<p>当前无聊天对话<p>";
-            //     } else {
-            //         let data = response.data;
-            //         data.forEach(item => {
-            //             if(item.infoType == "R") {                                   // 机器人问
-            //                 chatDetail += '<div class="say" style="position: relative;width: 420px;background: #ebebeb;overflow: hidden; padding: 5px 0;"><div class="say_svg common_svg"  style="position: absolute; width: 14px; height: 14px;top: 8px;left: 0px;"><svg viewBox="0 0 120 180" id="trigon-left" width="100%" height="100%" ><path fill="#fff" d="M104.555,178.368l-1.313-1.013l-97.863-76.5C2.064,98.269,0,94.387,0,90.056c0-4.332,2.126-8.212,5.378-10.8l97.676-76.443l1.626-1.294C106.243,0.563,108.119,0,110.12,0c5.44,0,9.88,4.162,9.88,9.338l0,0v161.324l0,0c0,5.175-4.439,9.338-9.88,9.338C108.057,180,106.118,179.381,104.555,178.368L104.555,178.368z"></path></svg></div><div class="say_text common_text" style="max-width: 390px;padding: 6px; border: 1px solid #d9d9d9;border-radius: 8px;font-size: 14px;line-height: 18px;color: #333;word-break: break-all;margin-left: 10px; background: #fff;">'+item.speakText+'<span style="font-size: 12px; color: #38F; padding-left: 10px;">'+item.mathRule+'</span>'+'</div></div>';
-            //             } else if(item.infoType == "M") {                            // 客户回答
-            //                 chatDetail += '<div class="replay" style="position: relative;width: 420px;background: #ebebeb;overflow: auto; padding: 5px 0;"><div class="replay_svg common_svg"  style="position: absolute; width: 14px; height: 14px; right: 0px;top: 8px;"><svg viewBox="0 0 120 180" id="trigon-left" width="100%" height="100%" ><path fill="#9fe658" d="M15.446,1.631l1.313,1.013l97.863,76.5c3.313,2.587,5.378,6.469,5.378,10.8c0,4.332-2.126,8.212-5.378,10.8l-97.676,76.443l-1.626,1.294C13.757,179.438,11.881,180,9.88,180C4.44,180,0,175.838,0,170.662l0,0V9.337l0,0C0,4.163,4.44,0,9.88,0C11.944,0,13.882,0.619,15.446,1.631L15.446,1.631z"></path></svg></div><div class="replay_text common_text" style="max-width: 390px;padding: 6px; border: 1px solid #d9d9d9;border-radius: 8px;font-size: 14px;line-height: 18px;color: #333;word-break: break-all;float: right;margin-right: 10px; background: #9fe658;">'+item.speakText+'<span style="font-size: 12px; color: #38F">'+item.mathRule+'</span>'+'</div></div>';
-            //             }
-            //         });
-            //     }
-            //     _this.$notify({
-            //         title: '聊天内容',
-            //         duration: 0,
-            //         dangerouslyUseHTMLString: true,
-            //         message: chatDetail
-            //     });
-            // });
+        },
+        playVoice(refIndex, item) {
+            item.imgFlag = true;                                // 当前图片动画
+            this.$refs[refIndex][0].play();                     // 当前录音播放
+        },
+        ended(item) {
+            item.imgFlag = false;
         }
+
     }
 };
 </script>
@@ -291,5 +250,66 @@ export default {
     }
     .right {
         background: rebeccapurple;
+    }
+    
+    // 聊天窗口
+    .yi-wrap {
+        padding: 10px 15px 0 15px;
+        margin-top: -15px;
+        border-top: 1px solid #ccc;
+        max-height: 60vh;
+        overflow: auto;
+    }
+    .say {
+        position: relative;
+        margin-top: 20px;
+        margin-bottom: 20px;
+        width:100%;
+    }
+    .replay {
+        position: relative;
+        width: 100%;
+        margin-top: 20px;
+    }
+    .common-text {
+        max-width: 580px;
+        padding: 6px;
+        border: 1px solid #d9d9d9;
+        border-radius: 8px;
+        font-size: 14px;
+        line-height: 20px;
+        color: #333;
+    }
+    .replay-content {
+        float: right;
+        margin-right: 10px;
+        word-break: break-all;
+        background: #9fe658;
+        cursor: pointer;
+    }
+    .replay-time{
+        position: absolute;
+        right: 14px;
+        top: -20px;
+    }
+
+    // 录音效果
+    @keyframes onceVoiceAnimation {
+        0% {
+            opacity: 1;
+        } 50% {
+            opacity: 0;
+        } 100% {
+            opacity: 1;
+        }
+    }
+
+    .voice {
+        animation: onceVoiceAnimation 1.6s ease-in-out infinite;
+    }
+    .wav {
+        height: 0px;
+        opacity: 0;
+        width: 1px;
     }
 </style>
